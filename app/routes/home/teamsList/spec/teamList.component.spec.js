971d446c91a6120407c8a0c1e8fa8782c03a3edf
Kaj Bialas <kbialas@apptension.com>
2017-04-19 15:06:32 +0200

    Temp teamsList.component spec

---
+++ app/routes/home/teamsList/spec/teamList.component.spec.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import { spy } from 'sinon';
+import { FormattedMessage } from 'react-intl';
+import { fromJS } from 'immutable';
+
+import Team from '../../team/team.component';
+import TeamsList from '../teamsList.component';
+
+
+
+
+describe('Teams List Component', function () {
+  const defaultProps = {
+    items: fromJS({ a: 'a', b: 'b', c: 'c' }),
+  };
+
+  const component = (props = {}) => (
+    <TeamsList {...defaultProps} {...props} />
+  );
+
+  it('should render Team root', function () {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.teams-list'))
+      .to.have.length(1);
+  });
+});
