1faf47636da5d51a4da9294c57c189ce8d70d1fa
Kaj Bialas <kbialas@apptension.com>
2017-04-19 15:06:05 +0200

    Temp spec for team.component

---
+++ app/routes/home/team/spec/team.component.spec.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import { spy } from 'sinon';
+import { fromJS } from 'immutable';
+
+import Team from '../team.component';
+import TeamsList from '../../teamsList/teamsList.component';
+
+describe('Team Component', function () {
+  const defaultProps = {
+    data: fromJS({ crestUrl: 'a', name: 'b', squadMarketValue: 'c' }),
+  };
+
+  const component = (props = {}) => (
+    <Team {...defaultProps} {...props} />
+  );
+
+  it('should render Team root', function () {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.team'))
+      .to.have.length(1);
+  });
+});
