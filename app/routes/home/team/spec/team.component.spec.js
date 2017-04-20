ccd893484f1692c437a4a4444bc5f9d56cfb7c3b
Kaj Bialas <kbialas@apptension.com>
2017-04-20 10:20:40 +0200

    Spec for test component

--- app/routes/home/team/spec/team.component.spec.js
+++ app/routes/home/team/spec/team.component.spec.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { spy } from 'sinon';
 import { fromJS } from 'immutable';
 
 import Team from '../team.component';
-import TeamsList from '../../teamsList/teamsList.component';
 
 describe('Team Component', () => {
   const defaultProps = {
-    data: fromJS({ crestUrl: 'a', name: 'b', squadMarketValue: 'c' }),
+    data: fromJS({ crestUrl: 'http://apptension.com', name: 'test-name', squadMarketValue: 'test-value' }),
   };
 
   const component = (props = {}) => (
@@ -18,7 +16,36 @@ describe('Team Component', () => {
 
   it('should render Team root', () => {
     const wrapper = shallow(component());
-    expect(wrapper.find('.team'))
-      .to.have.length(1);
+    expect(wrapper.find('.team')).to.have.length(1);
+  });
+
+  it('should render team logo', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.team-logo')).to.have.length(1);
+  });
+
+  it('should render team name', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.team-name')).to.have.length(1);
+  });
+
+  it('should render team value', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.team-value')).to.have.length(1);
+  });
+
+  it('should insert correctly values in team-logo style', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.team-logo')).to.have.style('background-image', 'url(http://apptension.com)');
+  });
+
+  it('should insert correctly values in team-name', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.team-name').text()).to.be.equal('test-name,');
+  });
+
+  it('should insert correctly values in team-value', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.team-value').text()).to.be.equal('test-value');
   });
 });
