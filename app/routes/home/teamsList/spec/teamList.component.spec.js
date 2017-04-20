714664041e0e29a68ea8bf46e8bb896904092d92
Kaj Bialas <kbialas@apptension.com>
2017-04-20 11:11:35 +0200

    Spec for teamList component

--- app/routes/home/teamsList/spec/teamList.component.spec.js
+++ app/routes/home/teamsList/spec/teamList.component.spec.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
-import { spy } from 'sinon';
 import { FormattedMessage } from 'react-intl';
 import { fromJS } from 'immutable';
 
 import Team from '../../team/team.component';
 import TeamsList from '../teamsList.component';
 
-
-
-
 describe('Teams List Component', () => {
   const defaultProps = {
-    items: fromJS({ a: 'a', b: 'b', c: 'c' }),
+    items: fromJS({ testValue1: 'test-value-1', testValue2: 'test-value-2', testValue3: 'test-value-3' }),
   };
 
   const component = (props = {}) => (
@@ -24,5 +20,25 @@ describe('Teams List Component', () => {
     const wrapper = shallow(component());
     expect(wrapper.find('.teams-list'))
       .to.have.length(1);
+  });
+  it('should render team list title', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.teams-list__title'))
+      .to.have.length(1);
+  });
+  it('should render team list items', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find('.teams-list__items'))
+      .to.have.length(1);
+  });
+  it('should render message', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find(FormattedMessage))
+      .to.have.length(1);
+  });
+  it('should pass data into Team', () => {
+    const wrapper = shallow(component());
+    expect(wrapper.find(Team).first().prop('data'))
+      .to.be.equal(defaultProps.items.get('testValue1'));
   });
 });
